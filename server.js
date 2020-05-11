854737640b4ca3fac0461d678436099dfd2dbbcb
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-05-11 15:53:48 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
+const fetch = require('node-fetch');
+
+const app = express();
+app.use(bodyparser.json());
+app.use(cors());
+
+app.get('/', (req,res) => res.send('this is working'))
+
+app.listen(process.env.PORT || 3001, () => console.log(`app is running on port ${process.env.PORT} or 3001`))
